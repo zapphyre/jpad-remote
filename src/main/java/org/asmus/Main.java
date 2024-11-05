170a651bc28bef8ce29c0d554c9616ec7d0e8edd
Marek Manco <marek.manco@schlieger.cz>
2024-11-05 22:48:03 +0100

    key filter;

--- src/main/java/org/asmus/Main.java
+++ src/main/java/org/asmus/Main.java
@@ -3,8 +3,7 @@ package org.asmus;
 import lombok.extern.slf4j.Slf4j;
 import org.asmus.service.JoyWorker;
 import org.asmus.yt.model.Gamepad;
-
-import java.util.concurrent.Executors;
+import reactor.core.publisher.Flux;
 
 @Slf4j
 public class Main {
@@ -12,8 +11,14 @@ public class Main {
     public static void main(String[] args) {
         JoyWorker joyWorker = new JoyWorker();
 
-        joyWorker.hookOnJoy("/dev/input/js0")
+        Flux<Gamepad> gamepadFlux = joyWorker.hookOnJoy("/dev/input/js0");
+        gamepadFlux
                 .map(Gamepad::toString)
                 .subscribe(log::info);
+
+        gamepadFlux
+                .filter(Gamepad::isA)
+                .contextWrite(q -> q)
+                .subscribe(g -> log.info("now A"));
     }
 }

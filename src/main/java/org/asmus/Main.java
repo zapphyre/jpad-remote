8cc6a2c3418222ca2c23d53593ae62dcec86941f
Marek Manco <marek.manco@schlieger.cz>
2024-11-05 21:35:01 +0100

    increase karma;

--- src/main/java/org/asmus/Main.java
+++ src/main/java/org/asmus/Main.java
@@ -12,10 +12,7 @@ public class Main {
     public static void main(String[] args) {
         JoyWorker joyWorker = new JoyWorker();
 
-        Executors.newSingleThreadScheduledExecutor()
-                .execute(() -> joyWorker.hookOnJoy("/dev/input/js0"));
-
-        joyWorker.getGamepadEvents()
+        joyWorker.hookOnJoy("/dev/input/js0")
                 .map(Gamepad::toString)
                 .subscribe(log::info);
     }

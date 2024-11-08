2f0421952086ebb2c0533a9ed17b59abda07aa0a
zapphyre <mmanco89@gmail.com>
2024-11-08 19:49:24 +0100

    clean comments;

--- src/main/java/org/asmus/Main.java
+++ src/main/java/org/asmus/Main.java
@@ -6,6 +6,7 @@ import org.asmus.model.QualifiedEType;
 import org.asmus.service.JoyWorker;
 import org.asmus.tool.EventMapper;
 import org.asmus.tool.GamepadIntrospector;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
@@ -20,32 +21,16 @@ public class Main {
         JoyWorker joyWorker = new JoyWorker();
         GamepadIntrospector gamepadIntrospector = new GamepadIntrospector();
 
-        Flux<Gamepad> gamepadFlux = joyWorker.hookOnJoy("/dev/input/js0");
-
-        gamepadFlux
+        Disposable disposable = joyWorker.hookOnJoy("/dev/input/js0")
                 .mapNotNull(gamepadIntrospector::introspect)
                 .filter(Predicate.not(List::isEmpty))
-//                .delayElements(Duration.ofMillis(420))
-//                .window(Duration.ofMillis(420))
-//                .flatMap(Flux::collectList)  // Collecting items in each window into a list
-//                .doOnNext(q -> log.info("click count: {}", q.size()))
                 .map(EventMapper::translateTimed)
-//                .bufferUntilChanged(QualifiedEType::getType)
-//                .buffer(Duration.ofMillis(500)) // Collect events within each 420ms period
                 .bufferTimeout(3, Duration.ofMillis(420))
                 .flatMap(events -> events.size() == 1 ?
                         Flux.just(events.getFirst()) : Flux.just(events.getLast()))
                 .map(Object::toString)
                 .subscribe(log::info);
 
-//        Disposable disposable = gamepadFlux
-//                .map(gamepadIntrospector::introspect)
-//                .sample(Duration.ofMillis(420))
-//                .filter(Predicate.not(List::isEmpty))
-//                .map(EventMapper::translate)
-//                .map(Object::toString)
-//                .subscribe(log::info);
-
-//        Runtime.getRuntime().addShutdownHook(new Thread(disposable::dispose));
+        Runtime.getRuntime().addShutdownHook(new Thread(disposable::dispose));
     }
 }

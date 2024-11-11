b85316735076a41b52d63a09f1526232aa64b9b2
zapphyre <mmanco89@gmail.com>
2024-11-11 22:46:15 +0100

    two streams for axis and buttons events;

--- src/main/java/org/asmus/service/JoyWorker.java
+++ src/main/java/org/asmus/service/JoyWorker.java
@@ -1,12 +1,11 @@
 package org.asmus.service;
 
-import org.asmus.function.GamepadInputGroupQuery;
-import org.asmus.model.Gamepad;
 import org.asmus.evt.EAxisGamepadEvt;
 import org.asmus.evt.EButtonGamepadEvt;
+import org.asmus.function.GamepadInputGroupQuery;
+import org.asmus.model.Gamepad;
 import org.asmus.model.GamepadDefinition;
 import org.asmus.model.GamepadStateStream;
-import org.asmus.model.Reducible;
 import org.bbi.linuxjoy.LinuxJoystick;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,11 +24,7 @@ public class JoyWorker {
     private static final Logger log = LoggerFactory.getLogger(JoyWorker.class);
     private final Sinks.Many<Gamepad> buttonStream = Sinks.many().multicast().directBestEffort();
     private final Sinks.Many<Gamepad> axisStream = Sinks.many().multicast().directBestEffort();
-    private Gamepad gamepad = Gamepad.builder().build();
-
-    public GamepadStateStream hookOnDefault() {
-        return hookOn(GamepadDefinition.builder().build());
-    }
+    private final Gamepad gamepad = Gamepad.builder().build();
 
     BiFunction<Gamepad, EButtonGamepadEvt, Gamepad> reducer(GamepadInputGroupQuery<Boolean> buttonQuery) {
         return (gamepad, evt) -> buttonQuery.getValueForIndex(evt.getNum())
@@ -40,6 +35,12 @@ public class JoyWorker {
         return idx -> witter -> witter.setTriggered(getter.apply(idx));
     }
 
+    BinaryOperator<Gamepad> laterMerger = (p, q) -> q;
+
+    public GamepadStateStream hookOnDefault() {
+        return hookOn(GamepadDefinition.builder().build());
+    }
+
     public GamepadStateStream hookOn(GamepadDefinition definition) {
         LinuxJoystick j = new LinuxJoystick(definition.getDev(), definition.getButtons(), definition.getAxis());
 
@@ -48,33 +49,31 @@ public class JoyWorker {
 
         j.open();
 
-//        ScheduledFuture<?> emiterCloseable = Executors.newSingleThreadScheduledExecutor()
-//                .scheduleAtFixedRate(() -> buttonStream.tryEmitNext(gamepad), 30, 20, TimeUnit.MILLISECONDS);
+        ScheduledFuture<?> pollerCloseable = Executors.newSingleThreadScheduledExecutor().schedule(() -> {
+            while (true) {
 
-        ScheduledFuture<?> pollerCloseable = Executors.newSingleThreadScheduledExecutor()
-                .schedule(() -> {
-                    while (true) {
+                j.poll();
 
-                        j.poll();
+                Gamepad gamepadBtn = Arrays.stream(EButtonGamepadEvt.values())
+                        .reduce(gamepad, (q, p) -> p.getReducer(buttonStateSetor).apply(q, p), laterMerger);
+                buttonStream.tryEmitNext(gamepadBtn);
 
-                        Gamepad gamepadBtn = Arrays.stream(EButtonGamepadEvt.values())
-                                .reduce(gamepad, (q, p) -> p.getReducer(buttonStateSetor).apply(q, p), laterMerger);
-                        buttonStream.tryEmitNext(gamepadBtn);
+                Gamepad gamepadAxs = Arrays.stream(EAxisGamepadEvt.values())
+                        .reduce(gamepad, (q, p) -> p.getReducer(axisStateSetor).apply(q, p), laterMerger);
+                axisStream.tryEmitNext(gamepadAxs);
+            }
+        }, 0, TimeUnit.MILLISECONDS);
 
-                        Gamepad gamepadAxs = Arrays.stream(EAxisGamepadEvt.values())
-                                .reduce(gamepad, (q, p) -> p.getReducer(axisStateSetor).apply(q, p), laterMerger);
-                        axisStream.tryEmitNext(gamepadAxs);
-
-                    }
-                }, 0, TimeUnit.MILLISECONDS);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            pollerCloseable.cancel(true);
+            axisStream.tryEmitComplete();
+            buttonStream.tryEmitComplete();
+            j.close();
+        }));
 
         return GamepadStateStream.builder()
                 .axisFlux(axisStream.asFlux())
                 .buttonFlux(buttonStream.asFlux())
                 .build();
     }
-
-    BinaryOperator<Gamepad> laterMerger = (p, q) -> q;
-
-
 }

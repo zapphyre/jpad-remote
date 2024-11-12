8b11518eeb3d4a9638f84d45fb08068a518aa32f
Marek Manco <marek.manco@schlieger.cz>
2024-11-12 09:58:21 +0100

    removed one import; good day!

--- src/main/java/org/asmus/service/JoyWorker.java
+++ src/main/java/org/asmus/service/JoyWorker.java
@@ -15,7 +15,6 @@ import java.util.Arrays;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 

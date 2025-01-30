c1f9d551fca4c95830c7ee13001a85da2c6a3cfd
Marek Manco <marek.manco@schlieger.cz>
2025-01-30 16:32:40 +0100

    filter out modifier events after modified button release;

--- src/main/java/org/asmus/tool/GamepadIntrospector.java
+++ src/main/java/org/asmus/tool/GamepadIntrospector.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
-import org.asmus.model.Gamepad;
 import org.asmus.model.ButtonClick;
+import org.asmus.model.Gamepad;
 import org.asmus.model.TimedValue;
 
 import java.beans.IntrospectionException;
@@ -16,25 +16,23 @@ import java.util.*;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 @Slf4j
 @UtilityClass
 public class GamepadIntrospector {
 
-    Map<String, TimedValue> defaults = new HashMap<>();
     Map<String, TimedValue> values = new HashMap<>();
 
     @Getter
     Set<TimedValue> holding = new LinkedHashSet<>();
+    Set<String> modifiers = new HashSet<>();
 
     static {
         try {
             Arrays.stream(Introspector.getBeanInfo(Gamepad.builder().build().getClass()).getPropertyDescriptors())
                     .map(toTimedValueFor(Gamepad.builder().build()))
-                    .forEach(q -> defaults.put(q.getName(), q));
+                    .forEach(q -> values.put(q.getName(), q));
 
-            values.putAll(defaults);
             log.info("introspector initialized #{} states of Gamepad buttons", values.size());
         } catch (IntrospectionException e) {
             throw new RuntimeException(e);
@@ -54,34 +52,32 @@ public class GamepadIntrospector {
     };
 
     @SneakyThrows
-    public Stream<ButtonClick> introspect(Gamepad gamepad) {
+    public ButtonClick releaseEvent(Gamepad gamepad) {
         return Arrays.stream(Introspector.getBeanInfo(gamepad.getClass()).getPropertyDescriptors())
                 .map(toTimedValueFor(gamepad))
-                .map(pairWithPreviousValue);
-    }
-
-    public ButtonClick releaseEvent(Gamepad gamepad) {
-        return introspect(gamepad)
+                .map(pairWithPreviousValue)
+                .filter(buttonStateChanged)
                 .filter(buttonWasPressedAndReleased)
+                .filter(notModifier)
                 .reduce(lastElement)
                 .map(q -> q.withModifiers(popAllModifiers()))
                 .orElse(null);
     }
 
     Set<TimedValue> popAllModifiers() {
-        HashSet<TimedValue> timedValues = new HashSet<>(holding);
-        holding.clear();
-        values.clear();
-        values.putAll(defaults);
-        return timedValues;
+        holding.stream()
+                .map(TimedValue::getName)
+                .forEach(modifiers::add);
+
+        return new HashSet<>(holding);
     }
 
     BinaryOperator<ButtonClick> lastElement = (p, q) -> q;
 
-    //first is always previous value of the button pressed
+    Predicate<ButtonClick> buttonStateChanged = q -> q.getPush().isValue() != q.getRelease().isValue();
+    Predicate<ButtonClick> notModifier = q -> !modifiers.remove(q.getPush().getName());
     Predicate<ButtonClick> buttonWasPressed = q -> holding.add(q.getPush());
     Predicate<ButtonClick> buttonWasReleased = q -> holding.remove(q.getRelease()) && holding.remove(q.getPush());
-
     Predicate<ButtonClick> buttonWasPressedAndReleased = buttonWasPressed.and(buttonWasReleased);
 
     Function<PropertyDescriptor, TimedValue> toTimedValueFor(Gamepad gamepad) {
@@ -91,10 +87,10 @@ public class GamepadIntrospector {
                 .build();
     }
 
-    Function<PropertyDescriptor, String> invokeRealSafe(Gamepad gamepad) {
+    Function<PropertyDescriptor, Boolean> invokeRealSafe(Gamepad gamepad) {
         return descriptor -> {
             try {
-                return descriptor.getReadMethod().invoke(gamepad).toString();
+                return Boolean.valueOf(descriptor.getReadMethod().invoke(gamepad).toString());
             } catch (IllegalAccessException | InvocationTargetException e) {
                 return null;
             }
